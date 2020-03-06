711802846a091d591922bc8c1209d2ee3e8ca960
Joseph Roybal <39634437+joeroybal@users.noreply.github.com>
2020-03-06 10:46:20 -0600

    added a brute force decrypt option for input text

--- caesarCipher/Main.py
+++ caesarCipher/Main.py
@@ -64,11 +64,11 @@ def decrypt_text(text, key):
         # transverse the plain text
         for i in range(len(text)):
             char = text[i]
-            # Encrypt uppercase characters in plain text
+            # decrypt uppercase characters in plain text
 
             if char.isupper():
                 result += chr((ord(char) - key - 65) % 26 + 65)
-            # Encrypt lowercase characters in plain text
+            # decrypt lowercase characters in plain text
             else:
                 result += chr((ord(char) - key - 97) % 26 + 97)
     else:
@@ -78,8 +78,22 @@ def decrypt_text(text, key):
     return print(result)
 
 
+
+def brute_force_decrypt(text):
+    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for key in range(len(LETTERS)):
+        translated = ''
+        for i in range(len(text)):
+            char = text[i]
+            if char.isupper():
+                translated += chr((ord(char) - key - 65) % 26 + 65)
+            else:
+                translated += chr((ord(char) - key - 97) % 26 + 97)
+        print("Key #%s: %s" % (key, translated))
+
+
 def decrypt_query():
-    response = input("Would you like to decrypt an existing file(1) or input text(2)")
+    response = input("Would you like to decrypt an existing file(1), input text(2), or brute force input text(3)")
     if response == '1':
         decrypt_file()
     elif response == '2':
@@ -87,16 +101,19 @@ def decrypt_query():
         key = int(input("Enter numerical value for shift cipher (1-25)"))
         encrypt_text(text, key)
         decrypt_text()
+    elif response == '3':
+        text = input("Enter the phrase you would like decrypted")
+        brute_force_decrypt(text)
     else:
         print("Please enter either a 1 or 2")
         decrypt_query()
 
 
 def origin():
-    response = input("Would you like to Encrypt or Decrypt?")
-    if response == 'Encrypt' or 'encrypt':
+    response = input("Would you like to Encrypt(1) or Decrypt(2)?")
+    if response == '1':
         encrypt_query()
-    elif response == 'Decrypt' or 'decrypt':
+    elif response == '2':
         decrypt_query()
     else:
         print("Please enter valid response")
